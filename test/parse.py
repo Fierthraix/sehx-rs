269bbe8cc90d92686869dd3e72568b1c1683c058
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2021-07-29 03:26:48 -0400

    Updated bin

--- test/parse.py
+++ test/parse.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
+"""
+from sys import argv
+from struct import pack
+
+hexes = open(argv[1], 'r').read()
+bin = open(argv[2], 'wb')
+for hex in hexes.split():
+    bin.write(pack('I', int(hex, base=16)))
+"""
 import argparse
 from pathlib import Path
+import struct
 import sys
 
 
 def read(file):
     for line in file:
         for chunk in line.split():
-            yield chunk
+            yield struct.pack('I', int(chunk, 16))
 
 
 def run(file):
-    byte_string = ''.join(chunk for chunk in read(file.open('r')))
-    my_hex = bytearray.fromhex(byte_string)
+    byte_string = [chunk for chunk in read(file.open('r'))]
+    my_hex = list(byte for byte in byte_string)
     sys.stdout.buffer.write(my_hex)
 
 
