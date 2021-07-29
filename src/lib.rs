f4cf19ea560516abce176d2d0a825b8a554dc395
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2021-07-29 01:59:05 -0400

    Commit v2

--- src/lib.rs
+++ src/lib.rs
@@ -7,35 +7,22 @@ pub type UA = u16;
 pub type UB = u32;
 pub type UD = u64;
 
-pub const SS: usize = 0x1000;
-pub const N: usize = SS / 4;
-
-pub fn sex(x: u8) -> u8 {
-    let mut u: u16 = x as u16;
-    u = (u & 0x000f) | ((u & 0x00f0) << 4);
-    u = u | 0x6060;
-    u as u8
-}
-
-pub fn sex_mut(x: &mut u8) {
+pub fn sehx(x: &u8) -> u16 {
     let mut u: u16 = *x as u16;
     u = (u & 0x000f) | ((u & 0x00f0) << 4);
-    *x = (u | 0x6060) as u8;
+    u | 0x6060
 }
 
-pub fn sexd(x: u32) -> u32 {
-    let mut u: u64 = x as u64;
-    u = (u & 0x000000000000ffff) | ((u & 0x00000000ffff0000) << 16);
-    u = (u & 0x000000ff000000ff) | ((u & 0x0000ff000000ff00) <<  8);
-    u = (u & 0x000f000f000f000f) | ((u & 0x00f000f000f000f0) <<  4);
-    u = u | 0x6060606060606060;
-    u as u32
-}
-
-pub fn sexd_mut(x: &mut u32) {
+pub fn sehxd(x: &u32) -> u64 {
     let mut u: u64 = *x as u64;
     u = (u & 0x000000000000ffff) | ((u & 0x00000000ffff0000) << 16);
     u = (u & 0x000000ff000000ff) | ((u & 0x0000ff000000ff00) <<  8);
     u = (u & 0x000f000f000f000f) | ((u & 0x00f000f000f000f0) <<  4);
-    *x = (u | 0x6060606060606060) as u32;
+    u | 0x6060606060606060
+}
+
+pub fn sehx_to_u8(buf: &[u16]) -> &[u8] {
+    unsafe {
+        std::mem::transmute::<&[u16], &[u8]>(buf) 
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,15 @@ use std::env;
 use std::fs;
 use std::io::{self, Write};
 
-use sehx::sex_mut;
+use sehx::{sehx, sehx_to_u8};
 
 fn main() {
     let filename = env::args().skip(1).next().unwrap();
-    let mut bytes = fs::read(filename).unwrap();
-
-    for mut byte in bytes.iter_mut() { 
-        sex_mut(&mut byte);
-    }
+    let bytes = fs::read(filename).unwrap();
+    let sehx_bytes: Vec<u16> = bytes.iter().map(|byte| sehx(byte)).collect();
 
     let stdout = io::stdout();
     let mut handle = stdout.lock();
 
-    handle.write(&bytes).unwrap();
+    handle.write(&sehx_to_u8(&sehx_bytes)).unwrap();
 }
